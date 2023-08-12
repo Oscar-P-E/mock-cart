ce11303ee2a53e7f043a9aec03460c2f38415872
Oscar El <opelm1@gmail.com>
2023-08-13 00:42:58 +1000

    Make navbar sticky

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
-        <div className="container max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl border min-h-screen mx-auto p-4">
+        <div className="container max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl border min-h-screen mx-auto p-4 pt-0">
           <Navbar />
           <div className="text-center">
             <Routes>
